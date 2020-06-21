b6a78ac9b68264eee38eb1a37de0409d23023850
alienx456 <estebanelias27@gmail.com>
2020-06-21 15:16:18 -0500

    exp fixed

--- cientific_ops.js
+++ cientific_ops.js
@@ -2,8 +2,7 @@ const sqrt = (num)  => {return Math.sqrt(num)}
 
 const log = (num)  => {return Math.log(num)}
 
-//const exp = (num)  => {return Math.exp(num)}
-const exp = (num1)  => {Math.random() * (10 - 5) + 5}
+const exp = (num)  => {return Math.exp(num)}
 
 const pow = (num1, num2)  => {return Math.pow(num1,num2)}
 
